8c33a57b3dcfbac09aa9d2b2036210be27ad01d8
MusienkoYuriy97 <97musienko@gmail.com>
2023-06-29 10:46:08 +0200

    finish project

--- src/main/java/com/example/beerconfigserver/BeerConfigServerApplication.java
+++ src/main/java/com/example/beerconfigserver/BeerConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class BeerCongigServerApplication {
+public class BeerConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeerCongigServerApplication.class, args);
+        SpringApplication.run(BeerConfigServerApplication.class, args);
     }
 
 }
